c70168d4be26a471b9d3465a99cedeb7fb85c4d8
sagarkale329 <sagarkale329@gmail.com>
2026-01-31 04:20:23 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+assets: {
+  slideshow: [
+    "PASTE_SLIDE1_URL",
+    "PASTE_SLIDE2_URL",
+    ...
+  ],
+
+  gallery: [
+    "PASTE_IMAGE1_URL",
+    ...
+  ],
+
+  videos: [
+    "PASTE_VIDEO1_URL",
+    ...
+  ]
+}
